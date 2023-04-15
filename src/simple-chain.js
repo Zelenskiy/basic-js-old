6e02e8e230f586cd16af6f39114ac7135c894a18
Zelenskiyas <zelensk1973@gmail.com>
2023-04-15 10:47:48 +0300

    feat: add simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,57 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  links: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.links.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    if (value === undefined) {
+      this.links.push("( )");
+    } else {
+      this.links.push(`( ${value} )`);
+    }
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (
+      position < 1 ||
+      position > this.links.length ||
+      typeof position !== "number" ||
+      !Number.isInteger(position)
+    ) {
+      this.links = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.links.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.links.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const result = this.links.join("~~");
+    this.links = [];
+    return result;
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
+
+// console.log(
+//   chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain()
+// );
+// console.log('( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )');
+// console.log('======================================================================');
