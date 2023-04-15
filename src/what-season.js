a66f57bec13661cdfe0d6993f37e308ca41f6dbe
Zelenskiyas <zelensk1973@gmail.com>
2023-04-15 16:51:53 +0300

    feat: work with what-season

--- src/what-season.js
+++ src/what-season.js
@@ -12,67 +12,69 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 
-// function getSeason(date) {
-//   if (!date) {
-//     throw new Error('Unable to determine the time of year!');
-//   }
-
-//   if (!(date instanceof Date && !isNaN(date.valueOf()))) {
-//     throw new Error('Invalid date!');
-//     // return 'Invalid date!'
-//   }
-
-//   if (isNaN(date.getTime())) {
-//     throw new Error('Invalid date!');
-//   }
-
-//   if (new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate() < date.getDate()) {
-//     throw new Error('Invalid date!');
-//   }
-
-//   const month = date.getMonth();
-
-//   if (month === 11 || month <= 1) {
-//     return 'winter';
-//   } else if (month >= 2 && month <= 4) {
-//     return 'spring';
-//   } else if (month >= 5 && month <= 7) {
-//     return 'summer';
-//   } else {
-//     return 'autumn';
-//   }
-// }
-function getSeason(date="no-value") {
-  if (date === "no-value") {
-    return "Unable to determine the time of year!";
+function getSeason(date) {
+  if (!date) {
+    throw new Error('Unable to determine the time of year!');
   }
 
-  if (date instanceof Date) {
-    if (Object.prototype.toString.call(date) != "[object Date]")
-      throw new Error("Caught fake date!");
-
-    const month = date.getMonth();
-    switch (month) {
-      case 0:
-      case 1:
-      case 11:
-        return "winter";
-      case 2:
-      case 3:
-      case 4:
-        return "spring";
-      case 5:
-      case 6:
-      case 7:
-        return "summer";
-      case 8:
-      case 9:
-      case 10:
-        return "autumn";
-    }
+  if (!(date instanceof Date && !isNaN(date.valueOf()))) {
+    throw new Error('Invalid date!');
+    // return 'Invalid date!'
   }
-  throw new Error("Wrong argument passed!");
+
+  if (isNaN(date.getTime())) {
+    throw new Error('Invalid date!');
+  }
+
+  if (new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate() < date.getDate()) {
+    throw new Error('Invalid date!');
+  }
+
+  const month = date.getMonth();
+
+  if (month === 11 || month <= 1) {
+    return 'winter';
+  } else if (month >= 2 && month <= 4) {
+    return 'spring';
+  } else if (month >= 5 && month <= 7) {
+    return 'summer';
+  } else {
+    return 'autumn';
+  }
 }
+
+
+// function getSeason(date="no-value") {
+//   if (date === "no-value") {
+//     return "Unable to determine the time of year!";
+//   }
+
+//   if (date instanceof Date) {
+//     if (Object.prototype.toString.call(date) != "[object Date]")
+//       throw new Error("Caught fake date!");
+
+//     const month = date.getMonth();
+//     switch (month) {
+//       case 0:
+//       case 1:
+//       case 11:
+//         return "winter";
+//       case 2:
+//       case 3:
+//       case 4:
+//         return "spring";
+//       case 5:
+//       case 6:
+//       case 7:
+//         return "summer";
+//       case 8:
+//       case 9:
+//       case 10:
+//         return "autumn";
+//     }
+//   }
+//   throw new Error("Wrong argument passed!");
+// }
 
 module.exports = {
   getSeason,
